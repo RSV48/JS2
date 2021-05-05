6ce3ec5960a136b963ac23d8fffd8d614d19e00d
rsv <ena@enagent.ru>
2021-05-05 15:39:06 +0300

    js2_lesson_01

---
+++ script.js
@@ -0,0 +1,28 @@
+const goods = [
+    { title: 'Shirt', price: 150 },
+    { title: 'Socks', price: 50 },
+    { title: 'Jacket', price: 350 },
+    { title: 'Shoes', price: 250 },
+];
+
+
+
+const renderGoodsItem = (title, price) => {
+    return `<div class="goods-item">
+            <h3>${title}</h3>
+            <p>${price}</p>
+            <a href='#' class="button button_tex" type="button">Добавить</a>
+            </div>`;
+};
+
+const renderGoodsList = (element, list) => {
+    let goodsList = list.map(item => renderGoodsItem(item.title, item.price)).join('');
+    element.innerHTML = goodsList;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const elementList = document.querySelector('.goods-list');
+    renderGoodsList(elementList, goods);
+});
+
+
