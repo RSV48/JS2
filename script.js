9b7fc1ee89bf76d16b5f3ed9f2f9211c45cef07d
rsv <ena@enagent.ru>
2021-05-07 16:43:01 +0300

    Решение ДЗ lesson_02

--- script.js
+++ script.js
@@ -5,24 +5,71 @@ const goods = [
     { title: 'Shoes', price: 250 },
 ];
 
+class GodsItem {
+    constructor(title, price, quantity) {
+        this.title = title;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    render() {
+        return `<div class="goods-item">
+        <h3>${this.title}</h3>
+        <p>${this.price} ₽</p>
+        <p>${this.quantity} шт.</p>
+        <a href='#' class="button button_tex" type="button">Удалить</a>
+        </div>`;
+    }
+}
+class GoodsList {
+    constructor() {
+        this.goods = [];
+        this.total = {};
+    }
+    fetchGoods() {
+        this.goods = [
+            { title: 'Shirt', price: 150, quantity: 2 },
+            { title: 'Socks', price: 50, quantity: 4 },
+            { title: 'Jacket', price: 350, quantity: 1 },
+            { title: 'Shoes', price: 250, quantity: 3 },
+        ];
+    }
+    totalCost() {
+        this.total = { quantity: 0, cost: 0 };
+        this.goods.forEach(product => {
+            this.total.quantity += product.quantity;
+            this.total.cost += product.price * product.quantity;
+        })
+        const html = `<button class="basket" type="button">Корзина ${this.total.cost} ₽, ${this.total.quantity} шт. </button>`
+        const elementList = document.querySelector('.top_info');
+        elementList.innerHTML = html;
 
+    }
+    render() {
+        let listHtml = '';
+        this.goods.forEach(product => {
+            const productItem = new GodsItem(product.title, product.price, product.quantity);
+            listHtml += productItem.render();
+        });
+        const elementList = document.querySelector('.goods-list');
+        elementList.innerHTML = listHtml
 
-const renderGoodsItem = (title, price) => {
-    return `<div class="goods-item">
-            <h3>${title}</h3>
-            <p>${price}</p>
-            <a href='#' class="button button_tex" type="button">Добавить</a>
-            </div>`;
+    }
 };
 
-const renderGoodsList = (element, list) => {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price)).join('');
-    element.innerHTML = goodsList;
-}
+class addProduct {
+
+};
+
+class deleteProduct {
+
+};
+
 
 document.addEventListener('DOMContentLoaded', () => {
-    const elementList = document.querySelector('.goods-list');
-    renderGoodsList(elementList, goods);
+    const list = new GoodsList();
+    list.fetchGoods();
+    list.render();
+    list.totalCost();
 });
 
 
